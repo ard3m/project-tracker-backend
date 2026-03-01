eba5d644d27607e56c06248e834b30bfed0ef0ca
Adam Larosa <larosa.adl@gmail.com>
2026-03-02 00:15:11 +1100

    Refactored various attributes to have a consistent naming convention..
    • Identified the table attribute names which don't follow appropriate naming convention. For example image_uploaded_by becomes uploaded_by. • Edited the updated_by, updated_at and details attributes to the non-specific naming convention style, using ALTER TABLE queries. Schema diagram must be refreshed to see changes due to its cached storage.• Began writing the project.py file in models folder. • Discovered that you can't use any kind of quotation marks or apostrophes inside of a terminal git update

--- app/models/project.py
+++ app/models/project.py
@@ -1,15 +1,21 @@
-# app/models/user.py
+# app/models/project.py
+from typing import Optional
 from datetime import datetime
 from sqlalchemy import String, DateTime, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.db.database import Base
 
-class AppUser(Base):
-    __tablename__ = "app_user"
+class Project(Base):
+    __tablename__ = "project"
 
-    user_id: Mapped[int] = mapped_column(primary_key=True)
+    project_id: Mapped[int] = mapped_column(primary_key=True)
 
     account_id: Mapped[int] = mapped_column(ForeignKey("account.account_id", ondelete="CASCADE"),nullable=False)
+
+    project_name: Mapped[str] = mapped_column(String(100)), nullable=False, 
+
+
+
 
     user_email: Mapped[str] = mapped_column(String(320), nullable=False, unique=True, index=True)
     username: Mapped[str] = mapped_column(String(30), nullable=False, unique=True)
@@ -17,6 +23,6 @@ class AppUser(Base):
     first_name: Mapped[str] = mapped_column(String(50), nullable=False)
     last_name: Mapped[str] = mapped_column(String(50), nullable=False)
 
-    last_login_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default="now()")
+    last_login_time: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True))
 
     account: Mapped["Account"] = relationship("Account", back_populates="users")
